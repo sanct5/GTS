beb0da8ed5bc027f778eddb13b2c391995302919
sanct5 <130994767+sanct5@users.noreply.github.com>
2023-04-30 18:43:17 -0500

    NavBar
    Se implementó la barra de navegación

---
+++ Games/src/App.js
@@ -0,0 +1,12 @@
+import NavBar from "./Components/navbar/NavBar";
+
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Games/src/Components/navbar/NavBar.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { GiConsoleController } from "react-icons/gi";
+import { BsSearch } from "react-icons/bs";
+import "./NavBar.css"
+
+function NavBar() {
+    return (
+        <div>
+          <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#"><GiConsoleController id="icon-logo"/>Games</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="#">About Us</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Filtros</a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Mejor calificados</a></li>
+                  <li><a class="dropdown-item" href="#">Más descargados</a></li>
+                  <li><hr class="dropdown-divider"></hr></li>
+                  <li><a class="dropdown-item" href="#">Más recientes</a></li>
+                </ul>
+                  </li>
+                  <li class="nav-item">
+                  <a class="nav-link disabled">Los videojuegos pueden ser una fuente de inspiración y motivación para superar desafíos tanto dentro como fuera de la pantalla.</a>
+                  </li>
+                </ul>
+                <form class="d-flex" role="search">
+                  <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search"></input>
+                  <button class="btn btn-outline-success" type="submit"><BsSearch id="icon-Search"/></button>
+                </form>
+              </div>
+            </div>
+          </nav>
+        </div>
+    )
+}
+
+export default NavBar
